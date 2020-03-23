e91f676f6d1e497730ce1f406b4d419c4b055aa5
Darshan Adiga <adigadarshan@gmail.com>
2020-03-23 23:30:08 +0530

    Dependency parsing

--- src/entity_extractor.py
+++ src/entity_extractor.py
@@ -1,6 +1,8 @@
 import spacy
 import en_core_web_sm
+from spacy.pipeline import DependencyParser
 from indexer import ESHelper
+import csv
 
 sp_core_nlp = en_core_web_sm.load()
 
@@ -8,19 +10,40 @@ esh = ESHelper(rss_url_file='data/rss-urls.txt')
 articel_list = esh.search('{"query":{"bool":{"must":[{"match_all":{}}],"must_not":[],"should":[]}},"from":0,"size":10,"sort":[],"aggs":{}}')
 #print(articel_list)
 
-res_csv = []
+# Save entities
+ent_file = './data/sample_entities.csv'
+ent_file = open(ent_file, 'w')
+ent_writer = csv.DictWriter(ent_file, fieldnames=['token', 'label'])
+ent_writer.writeheader()
+
+# Save dependencies
+dep_file = './data/sample_dependencies.csv'
+dep_file = open(dep_file, 'w')
+dep_writer = csv.DictWriter(dep_file, fieldnames=['token', 'dep'])
+dep_writer.writeheader()
+
+def write_deps(parsed_sent, dep_writer):
+    for dp in parsed_sent:
+        if (len(dp.text.strip()) > 1 and dp.text.strip() !='\n'):
+            row = {}
+            row['token'] = dp.text
+            row['dep'] = dp.dep_ 
+            dep_writer.writerow(row)
+
+def write_ents(parsed_sent, ent_writer):
+    for ent in parsed_sent.ents:
+        if (len(ent.text.strip()) > 1 and ent.text.strip() !='\n'):
+            row = {}
+            row['token'] = ent.text.strip()
+            row['label'] = ent.label_
+            ent_writer.writerow(row)
+        
 for ar in articel_list:
     parsed_article = sp_core_nlp(ar['text'])
     for i,sent in enumerate(parsed_article.sents):
         #print(i,':',sent.text)
-        parsed_sent = sp_core_nlp(sent.text)
-        ents = [ent for ent in parsed_sent.ents ]
-        ents = [ent for ent in ents if (len(ent.text.strip()) > 1 and ent.text.strip() !='\n')]
-        res_csv.extend([ent.text.strip() + ',' + ent.label_ for ent in ents])
+        #parsed_sent = sp_core_nlp(sent.text)
+        write_deps(sent, dep_writer)
+        write_ents(sent, ent_writer)
 
-# Save to file
-with open('./data/sample_entities.csv', 'w') as ot:
-    for r in res_csv:
-        #print(r, ':', len(r))
-        ot.write(r + "\n")
-print('Saved {} entities to file'.format(len(res_csv)))
+print('Saved entities and dependencies to file')
